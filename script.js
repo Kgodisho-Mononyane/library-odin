b06ecd663c015870f30d676b973ebc042f476390
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2025-11-07 09:06:15 +0200

    add basic dialog box and event listener to the button for the dialog box

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const addBtn = document.querySelector("#add-btn")
+const container = document.querySelector("#container")
+const dialogBox = document.querySelector("#dialog")
+
+addBtn.addEventListener("click", () => {
+    dialogBox.showModal()
+})
+
 const myLibrary = [];
 
 function Book() {
